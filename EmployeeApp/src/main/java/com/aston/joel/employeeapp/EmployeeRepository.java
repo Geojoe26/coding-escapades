0e657549f1d8be34ead4dd69c39c01d4dafeb6b9
Joel George Ponthokkan <joelgeorge46@gmail.com>
2024-10-29 00:28:19 +0000

    Delete EmployeeApp directory

--- EmployeeApp/src/main/java/com/aston/joel/employeeapp/EmployeeRepository.java
+++
@@ -1,6 +0,0 @@
-package com.aston.joel.employeeapp;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-}
