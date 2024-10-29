7926329f6a31482d575e12384215e3fbf1bb3e64
Joel George Ponthokkan <joelgeorge46@gmail.com>
2024-10-29 00:13:32 +0000

    Delete Spring Boot/Calculator/challenge-main/challenge-main directory

--- Spring Boot/Calculator/challenge-main/challenge-main/src/main/java/com/digital/challenge/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.digital.challenge.repository;
-
-import com.digital.challenge.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
